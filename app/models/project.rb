3c5a8eb50d207f692b1e1296b90b9efca8a89125
Brian Scramlin <scramlo@hotmail.com>
2015-04-08 15:18:48 -0500

    Style Updates. Added Delete Buttons to Projects Page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,6 @@ class Project < ActiveRecord::Base
   validates :title, presence: true,
                     length: { minimum: 5 }
 
-  has_many :workers
-  has_many :steps
+  has_many :workers, dependent: :destroy
+  has_many :steps, dependent: :destroy
 end
