de68a60548c85cb684173061f48b08419fd6b985
Brian Scramlin <scramlo@hotmail.com>
2016-07-06 17:25:44 -0400

    Created CRUD action for Vendors along with necessary views. Should work now.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,55 @@
 class VendorsController < ApplicationController
   http_basic_authenticate_with name: "swc", password: "SpoWes1100"
   before_action :authenticate_trustee!
-  
+
   def index
+    @vendors = Vendor.all
   end
+
+  def show
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def new
+    @vendors = Vendor.all
+    @vendor = Vendor.new
+  end
+
+  def edit
+    @vendors = Vendor.all
+    @vendor = Vendor.find(params[:id])
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_params)
+    if
+      @vendor.save
+      redirect_to vendors_path
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @vendor = Vendor.find(params[:id])
+    if
+      @vendor.update(vendor_params)
+      redirect_to vendors_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @vendor = Vendor.find(params[:id])
+    @vendor.destroy
+    redirect_to vendors_path
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :info)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429143453) do
+ActiveRecord::Schema.define(version: 20160706203540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,13 @@ ActiveRecord::Schema.define(version: 20150429143453) do
 
   add_index "trustees", ["email"], name: "index_trustees_on_email", unique: true, using: :btree
   add_index "trustees", ["reset_password_token"], name: "index_trustees_on_reset_password_token", unique: true, using: :btree
+
+  create_table "vendors", force: :cascade do |t|
+    t.string   "name"
+    t.text     "info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "workers", force: :cascade do |t|
     t.string   "name"
